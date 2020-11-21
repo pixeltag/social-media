fa5be0ef5c44b7a77fcf114b6ba9358b7b2b8053
902519 <i.youssef.tqh@mof.gov.sa>
2020-11-21 16:49:36 +0200

    commit

---
+++ server/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB:
+    "mongodb+srv://classed:jo3pOiRDPqMKVAnm@cluster0.4x8os.mongodb.net/merng?retryWrites=true&w=majority",
+  SECRET_KEY: "sldfkjlsjfaa;d;askd;a",
+};
