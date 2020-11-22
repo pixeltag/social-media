0d9f7d4e74a55d27c9d3d4104af992992df974d7
Ihab Youssef <pixeltagnet@gmail.com>
2020-11-22 09:22:02 +0200

    Update config.js
    remove mongodb secret

--- server/config.js
+++ server/config.js
@@ -1,5 +1,4 @@
 module.exports = {
   MONGODB:
-    "mongodb+srv://classed:jo3pOiRDPqMKVAnm@cluster0.4x8os.mongodb.net/merng?retryWrites=true&w=majority",
   SECRET_KEY: "sldfkjlsjfaa;d;askd;a",
 };
